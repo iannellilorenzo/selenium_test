408077042f64158f21eb2e9acbd3693a7e8c7376
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-04-23 09:35:27 +0200

    Aggiungere i file di progetto.

---
+++ selenium_test/Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+class Program
+{
+    public static void Main()
+    {
+
+    }
+}
