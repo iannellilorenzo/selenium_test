d7f90ff4b4d50ccf52b25c28f417d9498191464b
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-04-23 09:48:31 +0200

    brainstorm

--- selenium_test/Program.cs
+++ selenium_test/Program.cs
@@ -8,6 +8,6 @@ class Program
 {
     public static void Main()
     {
-
+        // come link prendo https://stackoverflow.com/questions, da qui entro in ogni domanda e dopo che sono entrato ci prendo il titolo della domanda, il corpo della domanda, gli upvote, e gli stessi campi della risposta flaggata come utile
     }
 }
