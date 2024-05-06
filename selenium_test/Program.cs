1c085c11882ec262e72d6b6c0776a7015526d64a
Iannelli Lorenzo <iannelli.lorenzo.studente@itispaleocapa.it>
2024-05-06 18:13:53 +0200

    finito

--- selenium_test/Program.cs
+++ selenium_test/Program.cs
@@ -1,30 +1,97 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Text.Json;
+using Newtonsoft.Json;
 using System.Text.Json.Nodes;
+using OpenQA.Selenium.DevTools.V121.Audits;
+using OpenQA.Selenium.DevTools.V121.DOM;
+using System.Security.Principal;
+
+class Item
+{
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public string Upvotes { get; set; }
+    public string FlaggedAnswer { get; set; }
+}
 
 class Program
 {
     public static void Main()
     {
-        // come link prendo https://stackoverflow.com/questions, da qui entro in ogni domanda e dopo che sono entrato ci prendo il titolo della domanda, il corpo della domanda, gli upvote, e gli stessi campi della risposta flaggata come utile. id per cookies: onetrust-accept-btn-handler
         IWebDriver driver = new ChromeDriver();
-        string url = "https://stackoverflow.com/questions";
+        string url = "https://stackoverflow.com/questions"; // debug url = "https://stackoverflow.com/questions/11828270/how-do-i-exit-vim"
         driver.Navigate().GoToUrl(url);
         Thread.Sleep(3000);
+
+        // Accept cookies
         IWebElement cookies = driver.FindElement(By.Id("onetrust-accept-btn-handler"));
         cookies.Click();
 
-        IWebElement questions = driver.FindElement(By.Id("questions"));
-        List<string> quests = new();
-        for(int i = 0; i < quests.Count; i++)
+        // Get question elements
+        ReadOnlyCollection<IWebElement> questionElements = driver.FindElements(By.CssSelector(".s-post-summary"));
+
+        // Create a list to store the question objects
+        List<Item> items = new();
+        int index = 0;
+
+        // Iterate through each question
+        foreach (IWebElement questionElement in questionElements)
         {
-            quests.Add(questions.GetAttribute("div"));
+            if (index == 3)
+            {
+                break;
+            }
+
+            // Navigate inside the question
+            IWebElement a = questionElement.FindElement(By.TagName("a"));
+            driver.Navigate().GoToUrl(a.GetAttribute("href"));
+            Thread.Sleep(5000);
+
+            // Get question title
+            string questionTitle = driver.FindElement(By.Id("question-header")).Text;
+
+            // Get question body
+            string questionBody = driver.FindElement(By.ClassName("js-post-body")).Text;
+
+            // Get question upvotes
+            string questionUpvotes = driver.FindElement(By.ClassName("js-vote-count")).Text;
+
+            // Get flagged answers
+            string flaggedAnswer = "";
+            try
+            {
+                flaggedAnswer = driver.FindElement(By.ClassName("accepted-answer")).Text;
+            }
+            catch (NoSuchElementException)
+            {
+                flaggedAnswer = "Not found";
+            }
+
+            // Create a item object
+            Item item = new Item
+            {
+                Title = questionTitle,
+                Body = questionBody,
+                Upvotes = questionUpvotes,
+                FlaggedAnswer = flaggedAnswer
+            };
+
+            items.Add(item);
+
+            Thread.Sleep(3000);
+            driver.Navigate().Back();
+            index++;
         }
 
+        // Serialize the list of questions to JSON using Newtonsoft.Json
+        string json = JsonConvert.SerializeObject(items, Formatting.Indented);
+        File.WriteAllText("data.json", json);
 
+        // Print the JSON string
+        Console.WriteLine(json);
+
+        Thread.Sleep(5000);
         driver.Close();
     }
 }
